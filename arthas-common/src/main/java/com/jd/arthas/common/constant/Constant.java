c71c079f01f090443c590743309fdfe673fda7d2
daishiji <davieac@foxmail.com>
2017-09-26 08:58:43 +0800

    1. 添加了spring的controller和依赖 2. 添加了constant类 3.添加了日志打印文件

---
+++ arthas-common/src/main/java/com/jd/arthas/common/constant/Constant.java
@@ -0,0 +1,11 @@
+package com.jd.arthas.common.constant;
+
+public class Constant {
+    
+    public static final String SUCCESS = "success";
+
+    public static void main(String[] args) {
+
+    }
+
+}
