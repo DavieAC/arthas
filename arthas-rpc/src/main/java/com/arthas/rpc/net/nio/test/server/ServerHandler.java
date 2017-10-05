afccf2e4034b469e5247ad39f2597fcdace6e3d1
daishiji <davieac@foxmail.com>
2017-10-05 19:43:05 +0800

    1. 添加了对输入信息的打印和退出命令

--- arthas-rpc/src/main/java/com/arthas/rpc/net/nio/test/server/ServerHandler.java
+++ arthas-rpc/src/main/java/com/arthas/rpc/net/nio/test/server/ServerHandler.java
@@ -1,5 +1,7 @@
 package com.arthas.rpc.net.nio.test.server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -18,15 +20,34 @@ public class ServerHandler implements Runnable {
 
     @Override
     public void run() {
-        InetAddress address = null;
-        try {
-            address = this.socket.getInetAddress();
-        } catch (Exception e) {
-            logger.error("获取链接ip地址出错", e);
+
+        // 打印ip信息
+        InetAddress address = this.socket.getInetAddress();
+        if (address == null) {
+            logger.error("获取链接ip地址出错");
             return;
         }
         String ip = address.getHostAddress();
         logger.info("接收到链接,ip:{}", ip);
+
+        // 打印出所有的传入参数
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String body = null;
+            while ((body = reader.readLine()) != null) {
+                if (body.equals("exit")) {
+                    logger.info("读取到了退出命令");
+                    break;
+                }
+                logger.info("本次读取到了:{}", body);
+            }
+        } catch (Exception e) {
+            logger.error("读取链接传输内容出错", e);
+        }
+
+        logger.info("本次链接完成");
     }
 
 }
