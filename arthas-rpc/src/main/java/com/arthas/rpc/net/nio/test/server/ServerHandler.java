887b04850de41fe9226cd0309d6e7184cbd44c26
daishiji <davieac@foxmail.com>
2017-10-05 18:03:45 +0800

    1. 添加了bio模块对链接的处理(打印日志)

--- arthas-rpc/src/main/java/com/arthas/rpc/net/nio/test/server/ServerHandler.java
+++ arthas-rpc/src/main/java/com/arthas/rpc/net/nio/test/server/ServerHandler.java
@@ -1,8 +1,14 @@
 package com.arthas.rpc.net.nio.test.server;
 
+import java.net.InetAddress;
 import java.net.Socket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ServerHandler implements Runnable {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServerHandler.class);
 
     private Socket socket;
 
@@ -12,7 +18,15 @@ public class ServerHandler implements Runnable {
 
     @Override
     public void run() {
-        
+        InetAddress address = null;
+        try {
+            address = this.socket.getInetAddress();
+        } catch (Exception e) {
+            logger.error("获取链接ip地址出错", e);
+            return;
+        }
+        String ip = address.getHostAddress();
+        logger.info("接收到链接,ip:{}", ip);
     }
 
 }
