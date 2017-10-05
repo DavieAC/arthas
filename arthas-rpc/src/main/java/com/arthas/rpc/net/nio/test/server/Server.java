072bc2cb3426126a5a2d47815feb9a2f7b24b4fe
daishiji <davieac@foxmail.com>
2017-10-05 18:25:11 +0800

    1. 添加了线程池实现bio的服务端

--- arthas-rpc/src/main/java/com/arthas/rpc/net/nio/test/server/Server.java
+++ arthas-rpc/src/main/java/com/arthas/rpc/net/nio/test/server/Server.java
@@ -3,6 +3,10 @@ package com.arthas.rpc.net.nio.test.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,11 +15,17 @@ public class Server {
 
     private static final Logger logger = LoggerFactory.getLogger(Server.class);
 
-    public static void main(String[] args) {
+    private ThreadPoolExecutor executor = null;
 
-        int port = 8088;
+    {
+        BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(20);
+        executor = new ThreadPoolExecutor(10, 20, 60, TimeUnit.SECONDS, queue);
+    }
+
+    public void startLocalBioServer(int port) {
 
         ServerSocket server = null;
+
         try {
             server = new ServerSocket(port);
             logger.info("本机启动服务端,监听端口:{}", port);
@@ -24,12 +34,10 @@ public class Server {
             while (true) {
                 socket = server.accept();
 
+
                 logger.info("接受到请求");
-                // 替换为线程池实现
-                new Thread(new ServerHandler(socket)).start();
-
+                executor.execute(new ServerHandler(socket));
             }
-
         } catch (Exception e) {
             logger.error("服务端异常", e);
         } finally {
@@ -41,6 +49,13 @@ public class Server {
                 }
             }
         }
+    }
+
+    public static void main(String[] args) {
+
+        int port = 8088;
+        Server server = new Server();
+        server.startLocalBioServer(port);
     }
 
 }
